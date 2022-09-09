e62d41cccabcd107ef33981e9646842c1807305d
anwardwk <113156321+anwardwk@users.noreply.github.com>
2022-09-09 21:35:40 +0700

    Tugas Dasar pemrograman pertama
    semoga saya bisa mengikuti dari dasar banget karna saya belum punya basic dari komputer

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Anwar Dwiky Fitriyanto" << endl;
+    cout << "A11.2022.14611" << endl;
+    cout << "Kalimantan timur" << endl;
+    cout << "Saya bersekolah Di Universitas Dian Nuswantoro" <<endl;
+
+    return 0;
+}
